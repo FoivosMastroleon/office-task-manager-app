fa09d523dc253722b563d57e28ae5b203f116a70
Foivos Mastroleon <vakycek@gmail.com>
2026-05-30 15:54:35 +0300

    feat: create component files and modify services

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { AuthInterceptorService } from './shared/services/auth-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
   ]
 };
---
+++ frontend/src/app/shared/services/task.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { Task } from '../interfaces/task.interface';
+
+
+
+@Injectable({ providedIn: 'root' })
+export class TaskService {
+  private apiUrl = `${environment.apiURL}/tasks`;
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  getTaskById(id: string): Observable<Task> {
+    return this.http.get<Task>(`${this.apiUrl}/${id}`);
+  }
+
+  getTasksByBoardId(boardId: string): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.apiUrl}?boardId=${boardId}`);
+  }
+
+  getTasksByAssignee(userId: string): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.apiUrl}?assignedTo=${userId}`);
+  }
+
+  createTask(data: Partial<Task>): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, data);
+  }
+
+  updateTask(id: string, data: Partial<Task>): Observable<Task> {
+    return this.http.put<Task>(`${this.apiUrl}/${id}`, data);
+  }
+
+  deleteTask(id: string): Observable<Task> {
+    return this.http.delete<Task>(`${this.apiUrl}/${id}`);
+  }
+
+    restoreTask(id: string): Observable<Task> {
+    return this.http.patch<Task>(`${this.apiUrl}/${id}/restore`, {});
+  }
+
+    
+   updateTaskStatus(id: string, status: 'todo' | 'working_on_it' | 'done'): Observable<Task> {
+    return this.http.patch<Task>(`${this.apiUrl}/${id}/status`, { status });
+  }
+}
