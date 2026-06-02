902a83868ecd96b0bd76e948b0a40572fc1bc2ed
Foivos Mastroleon <vakycek@gmail.com>
2026-06-02 13:43:07 +0300

    feat: add users and boards components on frontend

--- frontend/src/app/components/boards/boards.ts
+++ frontend/src/app/components/boards/boards.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Navbar } from '../navbar/navbar';
+import { AuthService } from '../../shared/services/auth.service';
+import { BoardService } from '../../shared/services/board.service';
+import { Board } from '../../shared/interfaces/board.interface';
 
 @Component({
   selector: 'app-boards',
-  imports: [],
+  imports: [CommonModule, RouterModule, Navbar],
   templateUrl: './boards.html',
   styleUrl: './boards.scss',
 })
-export class Boards {}
+export class Boards implements OnInit {
+  private authService = inject(AuthService);
+  private boardService = inject(BoardService);
+
+  boards: Board[] = [];
+
+  get role() {
+    return this.authService.loggedInUser()?.role;
+  }
+
+  ngOnInit(): void {
+    this.boardService.getBoards().subscribe(b => this.boards = b);
+  }
+}
--- frontend/src/app/components/users/users.ts
+++ frontend/src/app/components/users/users.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../shared/services/auth.service';
+import { UserService } from '../../shared/services/user.service';
+import { IUser, IUserSummary } from '../../shared/interfaces/user.interface';
+import { Navbar } from '../navbar/navbar';
 
 @Component({
   selector: 'app-users',
-  imports: [],
+  imports: [CommonModule, RouterModule, Navbar],
   templateUrl: './users.html',
   styleUrl: './users.scss',
 })
-export class Users {}
+export class Users implements OnInit {
+  private authService = inject(AuthService);
+  private userService = inject(UserService);
+
+  users: IUser[] = [];
+  userSummaries: IUserSummary[] = [];
+
+  get role() {
+    return this.authService.loggedInUser()?.role;
+  }
+
+  ngOnInit(): void {
+    if (this.role === 'admin') {
+      this.userService.getUsers().subscribe(u => this.users = u);
+    } else if (this.role === 'manager') {
+      this.userService.getUserSummaries().subscribe(u => this.userSummaries = u);
+    }
+  }
+}
