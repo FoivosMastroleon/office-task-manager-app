08f19b09aff31b3f69c4e8290e0f50a3b424396a
Foivos Mastroleon <vakycek@gmail.com>
2026-06-04 17:21:01 +0300

    fix: apiURL configuarion - add render

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@
 
 
 export const environment = {
-  apiURL: 'https://your-backend.onrender.com/api',
+  apiURL: 'https://office-task-manager-app.onrender.com/api',
   googleClientId: '546628013569-nfpc9sdnp98jopfhvddsm9g1qd9iankb.apps.googleusercontent.com',
   weatherApiKey: '185089a2d1b1d3accfb5e6b753c3da1e'
 };
