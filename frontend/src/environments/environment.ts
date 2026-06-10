a02def53ebe6bf833668ec787d9af2865cf83f4c
Foivos Mastroleon <vakycek@gmail.com>
2026-06-10 22:11:30 +0300

    fix: revert apiURL to correct backend endpoint

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@
 
 
 export const environment = {
-  apiURL: 'https://office-task-manager-api.onrender.com/api',
+  apiURL: 'https://office-task-manager-app.onrender.com/api',
   googleClientId: '815778857636-lccb5tked3vcrfcgeo30281ggke6ikqh.apps.googleusercontent.com',
   weatherApiKey: '185089a2d1b1d3accfb5e6b753c3da1e'
 };
